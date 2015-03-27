6f2038b1eea4f31dd4a26e55ed3ebc3dc305af1f
Timothy Blattner <timothy.blattner@nist.gov>
2015-03-27 15:13:58 -0400

    Added NIST header

--- src/main/java/gov/nist/isg/mist/stitching/lib/exceptions/GlobalOptimizationException.java
+++ src/main/java/gov/nist/isg/mist/stitching/lib/exceptions/GlobalOptimizationException.java
@@ -1,3 +1,30 @@
+// ================================================================
+//
+// Disclaimer: IMPORTANT: This software was developed at the National
+// Institute of Standards and Technology by employees of the Federal
+// Government in the course of their official duties. Pursuant to
+// title 17 Section 105 of the United States Code this software is not
+// subject to copyright protection and is in the public domain. This
+// is an experimental system. NIST assumes no responsibility
+// whatsoever for its use by other parties, and makes no guarantees,
+// expressed or implied, about its quality, reliability, or any other
+// characteristic. We would appreciate acknowledgement if the software
+// is used. This software can be redistributed and/or modified freely
+// provided that any derivative works bear some notice that they are
+// derived from it, and any modified versions bear some notice that
+// they have been modified.
+//
+// ================================================================
+
+// ================================================================
+//
+// Author: tjb3
+// Date: Aug 1, 2013 3:58:49 PM EST
+//
+// Time-stamp: <Aug 1, 2013 3:58:49 PM tjb3>
+//
+//
+// ================================================================
 package gov.nist.isg.mist.stitching.lib.exceptions;
 
 public class GlobalOptimizationException extends Exception {
